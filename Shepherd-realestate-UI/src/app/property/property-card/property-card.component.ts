db51e1ddf472ef6c78dbbf09e81690f80aa1c9f2
ronel <rone>
2023-05-09 22:39:53 -0700

    Binding and passing data between component in Card

--- Shepherd-realestate-UI/src/app/property/property-card/property-card.component.ts
+++ Shepherd-realestate-UI/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component ({
     selector: 'app-property-card',
@@ -10,13 +10,7 @@ import { Component } from "@angular/core";
 )
 
 export class PropertyCardComponent {
-
-  Property: any = {
-    "id":1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":12000
-  }
+  @Input() property : any
   //@Input() property: IPropertyBase;
   //@Input() hideIcons: boolean;
 }
--- Shepherd-realestate-UI/src/app/property/property-list/property-list.component.ts
+++ Shepherd-realestate-UI/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit{
 
+  properties: Array<any> = [
+    {
+      "id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "id":2,
+      "Name":"Erose Villa",
+      "Type":"Villa",
+      "Price":12000
+    },
+    {
+      "id":3,
+      "Name":"Mark Hill",
+      "Type":"Flat",
+      "Price":15300
+    },
+    {
+      "id":4,
+      "Name":"Camela House",
+      "Type":"House",
+      "Price":11400
+    },
+    {
+      "id":5,
+      "Name":"Macro Home",
+      "Type":"House",
+      "Price":14300
+    },
+    {
+      "id":6,
+      "Name":"Pearl White",
+      "Type":"House",
+      "Price":18000
+    }
+  ]
+
+  constructor(){ }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
 }
